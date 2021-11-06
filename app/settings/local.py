b8d9e61c71e06d42fd91c1843dd1e85bc1cd4722
xalvaine <xalvaine@yandex-team.ru>
2021-11-07 01:09:18 +0300

    [build] Turn debug mode on

--- app/settings/local.py
+++ app/settings/local.py
@@ -1,8 +1,6 @@
 from .default import *
 from dotenv import dotenv_values
 
-DEBUG = True
-
 env = {**dotenv_values("./.envs/.local/.postgres.env"), **env}
 
 DATABASES["default"]["PASSWORD"] = env["POSTGRES_PASSWORD"]
