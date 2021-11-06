8efefeccbad55b5833d8e10e433ac18f26e70a05
xalvaine <xalvaine@yandex-team.ru>
2021-11-07 01:31:08 +0300

    [fix] Fix errors

--- app/settings/production.py
+++ app/settings/production.py
@@ -4,5 +4,3 @@ DATABASES["default"]["PASSWORD"] = env["POSTGRES_PASSWORD"]
 DATABASES["default"]["HOST"] = "postgres"
 
 ALLOWED_HOSTS = ["tasks.api.xlvn.ru"]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
