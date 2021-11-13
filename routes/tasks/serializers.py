07fdf93547fa58b22c8dc9bc6eb9245a66533ebd
xalvaine <xalvaine@gmail.com>
2021-11-14 01:04:11 +0300

    [feat] Add tasks create request

--- routes/tasks/serializers.py
+++ routes/tasks/serializers.py
@@ -1,8 +1,15 @@
-from .models import Task
 from rest_framework import serializers
+from .models import Task
+from routes.subjects.models import Subject
 
 
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
+    subject_id = serializers.IntegerField()
+
     class Meta:
         model = Task
-        fields = ["id", "name", "description", "subject"]
+        fields = ["id", "name", "description", "subject_id"]
+
+    def create(self, validated_data):
+        subject = Subject.objects.get(pk=validated_data.pop("subject_id"))
+        return Task.objects.create(**validated_data, subject=subject)
