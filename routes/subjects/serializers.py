8e0dfdc35dac603cd32cc5f16ba8c563edb182cd
xalvaine <bme15122000@gmail.com>
2021-11-20 23:36:13 +0300

    [refactor] Remove hyperlinked serializers

--- routes/subjects/serializers.py
+++ routes/subjects/serializers.py
@@ -4,13 +4,12 @@ from routes.tasks.serializers import TaskSerializer
 from rest_framework import serializers
 
 
-class SubjectSerializer(serializers.HyperlinkedModelSerializer):
+class SubjectSerializer(serializers.ModelSerializer):
     tasks = TaskSerializer(many=True, read_only=True)
-    group_id = serializers.IntegerField()
 
     class Meta:
         model = Subject
-        fields = ["id", "name", "description", "tasks", "group_id", "image"]
+        fields = "__all__"
 
     def create(self, validated_data):
         group = Group.objects.get(pk=validated_data.pop("group_id"))
--- routes/tasks/serializers.py
+++ routes/tasks/serializers.py
@@ -3,12 +3,10 @@ from .models import Task
 from routes.subjects.models import Subject
 
 
-class TaskSerializer(serializers.HyperlinkedModelSerializer):
-    subject_id = serializers.IntegerField()
-
+class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ["id", "name", "description", "subject_id", "attachments", "deadline"]
+        fields = "__all__"
 
     def create(self, validated_data):
         subject = Subject.objects.get(pk=validated_data.pop("subject_id"))
