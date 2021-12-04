118772e5a300b05bd853cfc5bfa7294c7661ed00
xalvaine <bme15122000@gmail.com>
2021-12-04 20:03:14 +0300

    [feat] Add permissions

--- routes/groups/views.py
+++ routes/groups/views.py
@@ -18,6 +18,22 @@ class GroupViewSet(
         return Group.objects.filter(membership__user=self.request.user)
 
 
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (
+            Membership.objects.get(
+                group_id=view.kwargs.get("group_pk"),
+                user=request.user.pk,
+            ).role
+            == "ADMIN"
+        )
+
+
+class IsItself(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return Membership.objects.get(pk=view.kwargs.get("pk")).user == request.user
+
+
 class MembershipViewSet(
     mixins.ListModelMixin,
     mixins.RetrieveModelMixin,
@@ -27,10 +43,16 @@ class MembershipViewSet(
 ):
     serializer_class = GroupMembershipSerializer
     queryset = Membership.objects.none()
-    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return Membership.objects.filter(group=self.kwargs["group_pk"])
+
+    def get_permissions(self):
+        if self.action == "destroy":
+            return [permissions.IsAuthenticated(), IsItself()]
+        elif self.action == "update" or self.action == "partial_update":
+            return [permissions.IsAuthenticated(), IsAdmin()]
+        return [permissions.IsAuthenticated()]
 
 
 class JoinGroupView(mixins.CreateModelMixin, viewsets.GenericViewSet):
